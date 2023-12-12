c8b806d5ebe4912e0114a70dcc598b4b728b82a8
my-git-subhajit <130735403+my-git-subhajit@users.noreply.github.com>
2023-12-12 11:06:39 +0530

    updated index.html file and made repository public

--- CheckCommitsGit1.py
+++ CheckCommitsGit1.py
@@ -1,35 +1,28 @@
 import requests
-import json
-import subprocess
 
 def get_commits(repo_url):
     response = requests.get(repo_url)
-    #check the commit
+    
     if response.status_code == 200:
         commits = response.json()
         return commits
     else:
+        print(f"Failed to get commits. Status code: {response.status_code}")
+        print(f"Response content: {response.content}")
         raise Exception("Could not get commits")
-    
+
 def main():
-    
     repo_url = "https://api.github.com/repos/my-git-subhajit/CI-CD-Pipeline-Project/commits"
     
-
     # Get the latest commits.
-    commits = get_commits(repo_url)
-
-    if commits: 
+    try:
+        commits = get_commits(repo_url)
         sha_latest = str(commits[0]["sha"])
-    # sha1_latest= str(commits[1]["sha"])
-
-    #if sha_latest:
- #       subprocess.call(['sh', '/home/ubuntu/deploy.sh'])
-        print (sha_latest)
-        #print(sha_remote)
-    else:
-        print("No Commits found.")
-    
+        print(sha_latest)
+        # You can perform additional actions with the latest commit SHA here.
+    except Exception as e:
+        print(f"Error: {e}")
+        print("No commits found or there was an issue fetching commits.")
 
 if __name__ == "__main__":
     main()
